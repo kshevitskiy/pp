93c7b1efec88415817c0ad812240a3a56b2d0820
Evgeniy Kshevitskyi <kshevitskiy@gmail.com>
2022-04-12 10:40:21 +0200

    Fix issues in typings and temporary diable type checking in test suites

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,25 @@
 /// <reference types="vitest" />
 
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { configDefaults } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src')
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src')
+      }
+    },
+    plugins: [vue()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'cypress/*', 'tests/e2e/*']
     }
-  },
-  plugins: [vue()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    exclude: [
-      ...configDefaults.exclude,
-      'cypress/*',
-      'tests/e2e/*',
-      'src/types/*'
-    ]
-  }
-})
+  })
+}
